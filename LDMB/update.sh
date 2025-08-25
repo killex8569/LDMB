dc2afb62e0995cfd4cdb310e422cc61638059790
killex8569 <alexandreanette@gmail.com>
2025-08-25 21:15:20 +0200

    Adding update.sh

---
+++ LDMB/update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+make_update() {
+    cd /tmp
+    rm -rf /tmp/LDMB
+    git clone https://github.com/killex8569/LDMB.git
+    cd LDMB/LDMB
+    chmod +x uninstall.sh ; ./uninstall.sh
+    chmod +x install.sh ; ./install.sh
+}
+
+
+update() {
+    clear
+    echo "Update LDMB ?"
+    read -rp "Y/n" choix
+    case $choix in
+        y|Y) make_update ;;
+        n|N) echo "Aborting update"; exit 0 ;;
+        *) echo "Invalid choice"; sleep 1 ; update ;;
+    esac
+}
+
+update
